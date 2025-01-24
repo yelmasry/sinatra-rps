8a75b2892b20e8762ce54fc8366b932dcc90ae1e
yelmasry <yarra.elmasry@gmail.com>
2025-01-24 22:51:08 +0000

    final rock paper scissors

--- app.rb
+++ app.rb
@@ -2,11 +2,11 @@ require "sinatra"
 require "sinatra/reloader"
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  erb(:homepage)
 end
+
+
+####ROCK 
 
 get ("/rock")do 
   moves = ["rock", "paper", "scissors"]
@@ -23,3 +23,38 @@ get ("/rock")do
   
   erb(:zebra)
 end 
+
+###### PAPER 
+
+get ("/paper")do 
+  moves = ["rock", "paper", "scissors"]
+
+  @comp_move = moves.sample 
+
+  if @comp_move == "paper"
+    @outcome = "tied"
+  elsif @comp_move == "rock" 
+    @outcome = "won"
+  elsif 
+    @outcome = "lost"
+  end 
+  
+  erb(:giraffe)
+end 
+
+##### SCISSORS 
+get ("/scissors")do 
+  moves = ["rock", "paper", "scissors"]
+
+  @comp_move = moves.sample 
+
+  if @comp_move == "scissors"
+    @outcome = "tied"
+  elsif @comp_move == "rock" 
+    @outcome = "lost"
+  elsif 
+    @outcome = "won"
+  end 
+  
+  erb(:elephant)
+end
