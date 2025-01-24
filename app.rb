7fe189346d65cfd55b8d40df91efbcdfd83e9af4
yelmasry <yarra.elmasry@gmail.com>
2025-01-24 22:33:28 +0000

    progress

--- app.rb
+++ app.rb
@@ -7,3 +7,19 @@ get("/") do
   <p>Define some routes in app.rb</p>
   "
 end
+
+get ("/rock")do 
+  moves = ["rock", "paper", "scissors"]
+
+  @comp_move = moves.sample 
+
+  if @comp_move == "rock"
+    @outcome = "tied"
+  elsif @comp_move == "paper" 
+    @outcome = "lost"
+  elsif 
+    @outcome = "won"
+  end 
+  
+  erb(:zebra)
+end 
